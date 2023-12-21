8af208e01a99f42cbed71d90de6d4e1429d46d8a
Mitzone19 <141164118+Mitzone19@users.noreply.github.com>
2023-12-21 09:04:06 +0200

    Final commit

--- src/main/java/ro/scoalainformala/finalProject/CurrencyExchangeController.java
+++ src/main/java/ro/scoalainformala/finalProject/CurrencyExchangeController.java
@@ -2,8 +2,11 @@ package ro.scoalainformala.finalProject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
 
 @Controller
 public class CurrencyExchangeController {
@@ -15,6 +18,25 @@ public class CurrencyExchangeController {
     public String showExchangeForm(Model model) {
         model.addAttribute("rates", currencyRateService.getCurrencyRates());
         return "exchangeForm";
+    }
+    @PostMapping("/convert")
+    public String convertCurrency(@RequestParam double amount,
+                                  @RequestParam String fromCurrency,
+                                  @RequestParam String toCurrency,
+                                  Model model) {
+        Map<String, Double> rates = currencyRateService.getCurrencyRates();
+
+        // Calculate the amount in RON first, then convert to the target currency
+        double amountInRon = fromCurrency.equals("RON") ? amount : (amount / rates.getOrDefault(fromCurrency, 1.0));
+        double convertedAmount = toCurrency.equals("RON") ? amountInRon : (amountInRon * rates.getOrDefault(toCurrency, 1.0));
+
+        model.addAttribute("convertedAmount", convertedAmount);
+        model.addAttribute("fromCurrency", fromCurrency);
+        model.addAttribute("toCurrency", toCurrency);
+        model.addAttribute("amount", amount);
+        model.addAttribute("rates", rates);
+
+        return "exchangeResult";
     }
 }
 
